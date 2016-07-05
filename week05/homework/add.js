03e050f3d0a2d83c67abe6d67fb85402f8989680
quodnon <feanorfeanturi@gmail.com>
2016-07-05 08:34:34 +0000

    kata-5

--- week05/homework/add.js
+++ week05/homework/add.js
@@ -1,7 +1,23 @@
 'use strict';
 
-const add =string=>{
-    
-}
-
+const add =str=>{
+    if(!str || str=='') return 0
+    let array= str.match(/^\/\/(.+)\n/g)
+    if (array){
+        str = str.substring(array[0].length,str.length);
+        var pattern = "["+array[0].substr(2,1)+"\n,]"
+    } else {
+        var pattern = '[,\n]'
+    }
+    let summs = str.split(new RegExp(pattern,'g'))
+       summs.every((el)=>{
+                    if(parseInt(el)>=0){
+                        return true
+                    } else{
+                        throw Error('negatives not allowed!')
+                    } 
+            })
+    return summs
+        .reduce((prev,curr)=>parseInt(prev,10)+parseInt(curr,10))
+} 
 module.exports = add
--- week05/homework/tests/test.js
+++ week05/homework/tests/test.js
@@ -11,4 +11,31 @@ describe('add',()=>{
         .be.a('function')
         .have.lengthOf(1)
     })
+    
+    it('should recieve 0,1 or 2 numbers',()=>{
+        add().should.equal(0)
+        add('').should.equal(0)
+        add('1,3').should.equal(4)
+        add('1,4,5').should.equal(10)
+    })
+    
+    it('should allow unlimited amount of numbers',()=>{
+        add('1,2,3,4,5,6,7,8,9,10,223').should.equal(278)
+    })
+    
+    it('should allow new lines or commas as separators',()=>{
+        add(`1,2,3,4,5
+        6,7
+        8,9,10,223`).should.equal(278)
+    })
+    it('should support  custom delimiters',()=>{
+        add(`//;
+1;2;3;4;5
+6,7
+8;9;10,223`).should.equal(278)
+    })
+    
+    it('should throw exception on negative',()=>{
+       (()=>{ add('-2,3')}).should.throw('negatives not allowed!')
+    })
 }) 
